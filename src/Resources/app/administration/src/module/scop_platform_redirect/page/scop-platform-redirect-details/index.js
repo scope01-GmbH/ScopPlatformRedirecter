c3db3ad1e665f30f653336a62547bf54274826f4
Dominik Rosskopf <dominik.rosskopf@MacBook-Pro-von-Dominik.local>
2021-10-11 15:17:47 +0200

    Changed some texts

--- src/Resources/app/administration/src/module/scop_platform_redirect/page/scop-platform-redirect-details/index.js
+++ src/Resources/app/administration/src/module/scop_platform_redirect/page/scop-platform-redirect-details/index.js
@@ -1,45 +1,45 @@
 import template from './scop-platform-redirect-details.html.twig';
 
-const { Component, Mixin } = Shopware;
+const {Component, Mixin} = Shopware;
 
 Component.register('scop-platform-redirect-details', {
-	template,
+    template,
 
-	inject: [
-		'repositoryFactory'
-	],
+    inject: [
+        'repositoryFactory'
+    ],
 
-	mixins: [
-		Mixin.getByName('notification')
-	],
+    mixins: [
+        Mixin.getByName('notification')
+    ],
 
 
-	metaInfo() {
-		return {
-			title: this.$createTitle()
-		};
-	},
+    metaInfo() {
+        return {
+            title: this.$createTitle()
+        };
+    },
 
-	data() {
-		return {
-			redirect: null,
-			isLoading: false,
-			processSuccess: false,
-			repository: null
-		};
-	},
+    data() {
+        return {
+            redirect: null,
+            isLoading: false,
+            processSuccess: false,
+            repository: null
+        };
+    },
 
-	created() {
-		this.repository = this.repositoryFactory.create('scop_platform_redirecter_redirect');
-		this.getRedirect();
-	},
+    created() {
+        this.repository = this.repositoryFactory.create('scop_platform_redirecter_redirect');
+        this.getRedirect();
+    },
 
-	methods: {
-		getRedirect() {
-			this.repository.get(this.$route.params.id, Shopware.Context.api).then((entity) => { this.redirect = entity; })
-		},
+    methods: {
+        getRedirect() {
+            this.repository.get(this.$route.params.id, Shopware.Context.api).then((entity) => {this.redirect = entity;})
+        },
 
-		onClickSave() {
+        onClickSave() {
             if (this.redirect.sourceURL === this.redirect.targetURL) {
                 this.createNotificationError({
                     title: this.$tc('scopplatformredirecter.general.errorTitle'),
@@ -47,23 +47,23 @@ Component.register('scop-platform-redirect-details', {
                 })
                 return;
             }
-			this.isLoading = true;
-			this.repository.save(this.redirect, Shopware.Context.api).then(() => {
-				this.getRedirect();
-				this.isLoading = false;
-				this.processSuccess = true;
-			}).catch((exception) => {
-				this.isLoading = false;
-				this.createNotificationError({
-					title: this.$tc('scopplatformredirecter.general.errorTitle'),
-					message: exception
-				})
-			});
-		},
+            this.isLoading = true;
+            this.repository.save(this.redirect, Shopware.Context.api).then(() => {
+                this.getRedirect();
+                this.isLoading = false;
+                this.processSuccess = true;
+            }).catch((exception) => {
+                this.isLoading = false;
+                this.createNotificationError({
+                    title: this.$tc('scopplatformredirecter.general.errorTitle'),
+                    message: exception
+                })
+            });
+        },
 
-		saveFinish(){
-			this.processSuccess = false;
-		}
-	}
+        saveFinish() {
+            this.processSuccess = false;
+        }
+    }
 
 });
