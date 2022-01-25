f6d8b5e541b59af27a2e661057c46b1234b3c45c
dominik.rosskopf <dominik.rosskopf@scope01.com>
2022-01-25 15:58:55 +0100

    SPS-179 Fixxed endless Redirect when Source and Target URL point to the same Path but are different SEO Pahts

--- src/Subscriber/RequestSubscriber.php
+++ src/Subscriber/RequestSubscriber.php
@@ -17,13 +17,13 @@ declare(strict_types=1);
 
 namespace Scop\PlatformRedirecter\Subscriber;
 
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\Event\BeforeSendResponseEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Event\BeforeSendResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class RequestSubscriber implements EventSubscriberInterface
@@ -67,12 +67,12 @@ class RequestSubscriber implements EventSubscriberInterface
      */
     public function redirectBeforeSendResponse(BeforeSendResponseEvent $event): void
     {
-        $requestUri = (string)$event->getRequest()->get('resolved-uri');
+        $requestUri = (string)$event->getRequest()->get("sw-original-request-uri");
         $storefrontUri = $event->getRequest()->get('sw-storefront-url');
         $requestBase = $event->getRequest()->getPathInfo();
         $requestBaseUrl = $event->getRequest()->getBaseUrl();
         $queryString = (string)$event->getRequest()->getQueryString();
-        $search = [];
+
         // Block overriding /admin and /api and widgets, so you can't lock out of the administration.
         if (\strpos($requestBase, "/admin") === 0) {
             return;
@@ -92,40 +92,18 @@ class RequestSubscriber implements EventSubscriberInterface
             $requestUri .= '?' . $queryString;
         }
 
-        // try to load the seo route
         $context = Context::createDefaultContext();
-        $redirects = $this->seoUrlRepository->search((new Criteria())
-            ->addFilter(new EqualsAnyFilter('pathInfo', [$requestUri])), $context);
 
-        // if found overwrite search term with the seo route
-        if ($redirects->count() !== 0) {
-            foreach ($redirects as $redirect) {
-                $requestBase = $redirect->getSeoPathInfo();
-                // Search for Redirect
-                $search[] = [
-                    $requestBaseUrl . '/' . $requestBase, // relative url with shopware 6 in sub folder: /public/Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53
-                    $requestBaseUrl . $requestBase, // relative url with shopware 6 in sub folder url is not shopware seo url: /public/test
-                    $storefrontUri . $requestBase, // absolute url with shopware 6 in sub folder, full url with host: http://shopware-platform.local/public/test1
-                    $storefrontUri . '/' . $requestBase, // absolute url with shopware 6 in sub folder, full url with host and slash at the end: http://shopware-platform.local/public/Freizeit-Elektro/Telefone/
-                    $requestBase, // relative url domain configured in public folder: /Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53 or /test4
-                    '/' . $requestBase, // absolute url domain configured in public folder: http://shopware-platform.local/Shoes-Baby/
-                    \substr($requestBase, 1), // e.g. "test"
-                ];
-            }
-        }
-        if (!empty($search)) {
-            $search = array_merge(...$search);
-        } else {
-            $search = [
-                $requestBaseUrl . '/' . $requestUri, // relative url with shopware 6 in sub folder: /public/Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53
-                $requestBaseUrl . $requestUri, // relative url with shopware 6 in sub folder url is not shopware seo url: /public/test
-                $storefrontUri . $requestUri, // absolute url with shopware 6 in sub folder, full url with host: http://shopware-platform.local/public/test1
-                $storefrontUri . '/' . $requestUri, // absolute url with shopware 6 in sub folder, full url with host and slash at the end: http://shopware-platform.local/public/Freizeit-Elektro/Telefone/
-                $requestUri, // relative url domain configured in public folder: /Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53 or /test4
-                '/' . $requestUri, // absolute url domain configured in public folder: http://shopware-platform.local/Shoes-Baby/
-                \substr($requestUri, 1), // e.g. "test"
-            ];
-        }
+        $search = [
+            $requestBaseUrl . '/' . $requestUri, // relative url with shopware 6 in sub folder: /public/Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53
+            $requestBaseUrl . $requestUri, // relative url with shopware 6 in sub folder url is not shopware seo url: /public/test
+            $storefrontUri . $requestUri, // absolute url with shopware 6 in sub folder, full url with host: http://shopware-platform.local/public/test1
+            $storefrontUri . '/' . $requestUri, // absolute url with shopware 6 in sub folder, full url with host and slash at the end: http://shopware-platform.local/public/Freizeit-Elektro/Telefone/
+            $requestUri, // relative url domain configured in public folder: /Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53 or /test4
+            '/' . $requestUri, // absolute url domain configured in public folder: http://shopware-platform.local/Shoes-Baby/
+            \substr($requestUri, 1), // e.g. "test"
+        ];
+
 
         // search for the redirect in the database
         $redirects = $this->repository->search((new Criteria())->addFilter(new EqualsAnyFilter('sourceURL', $search))->addFilter(new EqualsFilter('enabled', true))
