e0997c421d6cbdd84469b3a039b1b8d376f425a1
Bundit Nuntates <dit@data-horizon.com>
2025-07-28 14:36:40 +0700

    Fix CanonicalRedirectService constructor for Shopware 6.7 compatibility

--- src/Decorator/CanonicalRedirectServiceDecorator.php
+++ src/Decorator/CanonicalRedirectServiceDecorator.php
@@ -9,6 +9,8 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\OrFilter;
 use Shopware\Core\Framework\Routing\CanonicalRedirectService;
+use Shopware\Core\Framework\Script\Execution\Hook;
+use Shopware\Core\Framework\Extensions\ExtensionDispatcher;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,9 +34,9 @@ class CanonicalRedirectServiceDecorator extends CanonicalRedirectService
      */
     private SystemConfigService $configService;
 
-    public function __construct(CanonicalRedirectService $inner, SystemConfigService $configService, EntityRepository $redirectRepository)
+    public function __construct(CanonicalRedirectService $inner, SystemConfigService $configService, EntityRepository $redirectRepository, ExtensionDispatcher $extensionDispatcher)
     {
-        parent::__construct($configService);
+        parent::__construct($configService, $extensionDispatcher);
         $this->configService = $configService;
         $this->repository = $redirectRepository;
         $this->inner = $inner;
