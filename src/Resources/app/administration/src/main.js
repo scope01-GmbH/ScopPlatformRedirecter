ceb129bd40d1cd78f8ba9c1902dd713d92d04c68
patrick <patrick.pahlke@scope01.com>
2026-05-28 15:21:06 +0200

    SPS-531 add possibility to auto create redirects when deleting products

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,4 +1,5 @@
 import './app/component/scop-platform-redirect-auto-redirect-config';
+import './app/component/scop-platform-redirect-auto-redirect-delete-config';
 import './app/component/scop-platform-redirect-not-found-config';
 import './module/scop_platform_redirect';
 import './module/sw-import-export/component/sw-import-export-edit-profile-general';
